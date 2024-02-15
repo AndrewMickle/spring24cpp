d27c29b5e0eddbc64ce9a799f6cde3647c9204ab
AndrewMickle <157724483+AndrewMickle@users.noreply.github.com>
2024-02-15 09:52:38 -0600

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int age = 0;
+  string name;
+  cout << "Hello World!"<<endl<<"welcome to c++"<<endl;
+  cout << "Enter your age: ";
+  cin >> age;
+  cout << "I am "<<age<<" years old"<<endl;
+  cout << "Enter your name: ";
+  cin >> name;
+  cout << "My name is "<<name<<endl;
+}
